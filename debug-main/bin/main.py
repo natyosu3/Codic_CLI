d0c2ae8ba32d0afe665c5fa8196da0db2f7bda34
natyosu3 <nachi334825@gmail.com>
2023-11-24 00:49:01 +0900

    デコレータを追加

--- debug-main/bin/main.py
+++ debug-main/bin/main.py
@@ -15,42 +15,54 @@ parser.add_argument("-a", "--api_token", help="codicのapiキーを設定して�
 args = parser.parse_args()
 
 
+def check_config_existence(func):
+    def wrapper(*args, **kwargs):
+        if not(os.path.isfile(config_path)):
+            print("configuration file Not Found.")
+            print("Create configuration directory...")
+            if os.mkdir(Path(os.path.abspath(sys.argv[0])).parent.parent / "config"): print("Success.")
+            else:
+                print("Error. Could not create configuration directory.")
+                sys.exit()
+            
+            print("Create configuration file...")
+            try:
+                with open(config_path, "w") as f:
+                    f.write(yaml.safe_dump({"API_TOKEN": None,
+                                            "DEFAULT_CASING": "lower underscore"
+                                            }))
+            except:
+                print("Error. Could not create configuration file")
+                sys.exit()
+            print("Success.")
+
+        return func(*args, **kwargs)
+    return wrapper
+
+
+@check_config_existence
 def load_api_token():
-    if os.path.isfile(config_path):
-        with open(config_path, "r") as f:
-            API_TOKEN = yaml.safe_load(f)["API_TOKEN"]
-        if API_TOKEN:
-            return API_TOKEN
+    with open(config_path, "r") as f:
+        API_TOKEN = yaml.safe_load(f)["API_TOKEN"]
 
-        else:
-            print("API TOKENが設定されていません. 以下のコマンドを実行し, TOKENを設定してください.")
-            print("cordic -a xxxxxx(API TOKEN)")
-            sys.exit()
-
+    if API_TOKEN:
+        return API_TOKEN
     else:
-        with open(config_path, "w") as f:
-            f.write(yaml.safe_dump({"API_TOKEN": None,
-                                    "DEFAULT_CASING": "snake"
-                                    }))
         print("API TOKENが設定されていません. 以下のコマンドを実行し, TOKENを設定してください.")
         print("cordic -a xxxxxx(API TOKEN)")
         sys.exit()
 
 
+@check_config_existence
 def load_default_casing():
-    if os.path.isfile(config_path):
-        with open(config_path, "r") as f:
-            DEFAULT_CASING = yaml.safe_load(f)["DEFAULT_CASING"]
-        if DEFAULT_CASING:
-            return DEFAULT_CASING
+    with open(config_path, "r") as f:
+        DEFAULT_CASING = yaml.safe_load(f)["DEFAULT_CASING"]
 
-    else:
-        with open(config_path, "w") as f:
-            f.write(yaml.safe_dump({"API_TOKEN": None,
-                                    "DEFAULT_CASING": "lower underscore"
-                                    }))
+    if DEFAULT_CASING:
+        return DEFAULT_CASING
 
 
+@check_config_existence
 def api(text):
     url = "https://api.codic.jp/v1/engine/translate.json"
     headers = {
@@ -69,7 +81,7 @@ def api(text):
     else:
         print(f"Error: {response.status_code} - {response.text}")
 
-
+@check_config_existence
 def set_api_token(token):
     with open(config_path, "r+") as f:
         data = yaml.safe_load(f.read)
