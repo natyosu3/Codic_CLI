82d47e2a372d54750c184e82c4f512607bab6f27
natyosu3 <nachi334825@gmail.com>
2023-11-24 11:48:00 +0900

    helpを修正

--- debug-main/bin/main.py
+++ debug-main/bin/main.py
@@ -16,12 +16,12 @@ parser = argparse.ArgumentParser(
 parser.add_argument("text", help="変換したいテキストを入力してください.", type=str, default=None, nargs="?")
 parser.add_argument("-a", "--api_token", help="codicのapiキーを設定してください.")
 parser.add_argument("-c", "--casing", help="""
-                    利用したいケーシングを入力してください.
-                    lower underscore: hello_func
-                    upper underscore: HELLO_FUNC
-                    camel           : helloFunc
-                    pascal          : HelloFunc
-                    hyphen          : hello-func
+                    利用したいケーシングを入力してください. ※クォーテーションを付けてください.
+                    \"lower underscore\": hello_func
+                    \"upper underscore\": HELLO_FUNC
+                    \"camel\"           : helloFunc
+                    \"pascal\"          : HelloFunc
+                    \"hyphen \"         : hello-func
                     """)
 parser.add_argument("-cdc", "--change_default_casing", help="デフォルトのケーシングを変更します.")
 args = parser.parse_args()
