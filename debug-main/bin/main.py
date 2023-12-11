943a5e78d08230fb05189b1a5421ccefaf7c2e66
natyosu3 <nachi334825@gmail.com>
2023-12-11 22:40:40 +0900

    Update config_dir path in main.py

--- debug-main/bin/main.py
+++ debug-main/bin/main.py
@@ -5,7 +5,7 @@ import yaml
 from pathlib import Path
 import os, sys
 
-config_dir = Path(os.path.abspath(sys._MEIPASS)).parent / "config"
+config_dir = config_dir = Path(os.path.dirname(sys.executable)) / "config"
 config_path = config_dir / "config.yaml"
 
 parser = argparse.ArgumentParser(
